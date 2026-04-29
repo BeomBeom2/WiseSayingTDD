dc7dbaaa41a5504049aafde3929cc15028aeb7e3
BeomBeom2 <whwjdqja20@naver.com>
2026-04-29 17:02:26 +0900

    step-04-blue-1

--- src/test/java/com/back/AppTest.java
+++ src/test/java/com/back/AppTest.java
@@ -13,7 +13,7 @@ public class AppTest {
     @Test
     @DisplayName("`== 명언 앱 ==` 출력")
     void t1() {
-        String rs = AppTestRunner.run("종료");
+        String rs = AppTestRunner.run("");
 
         assertThat(rs)
                 .contains("== 명언 앱 ==");
@@ -26,7 +26,6 @@ public class AppTest {
                 등록
                 현재를 사랑하라.
                 작자미상
-                종료
                 """);
 
         assertThat(rs)
--- src/test/java/com/back/AppTestRunner.java
+++ src/test/java/com/back/AppTestRunner.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class AppTestRunner {
 
     public static String run(String str) {
-        Scanner scanner = TestUtil.genScanner(str);
+        Scanner scanner = TestUtil.genScanner(str + "\n종료");
 
         ByteArrayOutputStream output = TestUtil.setOutToByteArray();
         new App(scanner).run();
